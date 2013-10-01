ec0a8ae60dfafd4b602fb0a310931f14bead49c6
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-10-01 13:57:14 +0100

    Steps added.

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,20 @@
+Given(/^an empty bookshelf$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I search for a book$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^shows no results found$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a bookshelf with one book$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I search for a non existent book$/) do
+  expect ( result ).to be 0
+end
+
