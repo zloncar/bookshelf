6409a1c02ea489f87ba3c16c2c75f7032162f858
Nancy <nancy.richardson@onthebeach.co.uk>
2013-10-01 15:05:31 +0100

    first rspec test

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,7 +2,10 @@ require 'library'
 
 describe "Library" do 
 	it 'should add book' do
-		pending
-		@library = Library.new
+		library = Library.new
+		mysearch = "Grape"
+		library.add_book( mysearch )
+		result = library.search_by_title( mysearch )
+		expect(result).to eq(["Grape"])
 	end
 end
